f5b81ab6205538bbbcd6300835ba39efd9483a1d
Thiago Silva <thiago@nano.org>
2021-07-05 11:05:39 -0300

    Adding unittests + moving to CMake

---
+++ unittests/entry.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <helper_functions.hpp>
+
+int main (int argc, char ** argv)
+{
+	testing::InitGoogleTest (&argc, argv);
+	auto res = RUN_ALL_TESTS ();
+	delete_temp_db_path (get_temp_path());
+	return res;
+}
