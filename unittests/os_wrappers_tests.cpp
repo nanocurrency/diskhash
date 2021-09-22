339c01386d0a254b6f809980aa6cacf14e0a8cb4
Thiago Silva <thiago@nano.org>
2021-09-22 11:15:36 -0700

    Adds casting to const char*

--- unittests/os_wrappers_tests.cpp
+++ unittests/os_wrappers_tests.cpp
@@ -1,6 +1,8 @@
+#include <utility>
+#include <cassert>
+
 #include <os_wrappers.h>
 
-#include <assert.h>
 #include <helper_functions.hpp>
 
 void os_wrappers_dht_delete_file_works ();
@@ -18,7 +20,7 @@ int main (int argc, char ** argv)
 void os_wrappers_dht_delete_file_works ()
 {
 	auto file_path = unique_path() / "test_file.dht";
-	const char* file_path_str = file_path.c_str ();
+	const char* file_path_str = (const char*)(file_path.c_str ());
 	dht_open_file (file_path_str, O_RDWR | O_CREAT, false);
 	assert (db_exists (file_path_str));
 	dht_delete_file (file_path_str);
@@ -28,7 +30,7 @@ void os_wrappers_dht_delete_file_works ()
 void os_wrappers_dht_open_file_creates_file ()
 {
 	auto file_path = unique_path() / "test_file.dht";
-	const char* file_path_str = file_path.c_str ();
+	const char* file_path_str = (const char*)(file_path.c_str ());
 	dht_file_t file_descriptor = dht_open_file (file_path_str, O_RDWR | O_CREAT, false);
 	assert (file_descriptor > 0);
 	assert (db_exists (file_path_str));
